7a7104d9e3049524e495197908c4e159a6b81104
Sarvagya Gupta <sarvagyagupta2@gmail.com>
2023-03-23 23:30:24 +0530

    Changed the filetype from txt to cpp

---
+++ 001_powerOfTwo.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }else if ((n & (n-1)) == 0){
+            return true ;
+        }else{
+            return false;
+        }    
+    }
+};
